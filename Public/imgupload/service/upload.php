84f4fed0baad4c4c985c08927d304deedcac98ff
thevet <45598021@qq.com>
2017-04-05 09:05:07 +0800

    Update upload.php
    修复图片上传路径错误的问题,这次没问题了

--- Public/imgupload/service/upload.php
+++ Public/imgupload/service/upload.php
@@ -3,7 +3,7 @@
         //获取json配置文件
         $CONFIG = json_decode(preg_replace("/\/\*[\s\S]+?\*\//", "", file_get_contents("config.json")), true);
         //拼接绝对上传路径
-        $upFilePath = $_SERVER['DOCUMENT_ROOT']."".$CONFIG['uploadFile'];
+        $upFilePath = $_SERVER['DOCUMENT_ROOT']."/".$CONFIG['uploadFile'];
         //配置文件名称
         $rename = md5(time()).'.jpg';
         //限制上传格式
